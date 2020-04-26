6ec4b1a17c01616df5141c2e06edf80a85bce934
brunobozz <brunobozz@gmail.com>
2020-04-26 09:27:48 -0300

    serviço da api melhorado

--- src/app/modules/newsapi/services/newsapi.service.ts
+++ src/app/modules/newsapi/services/newsapi.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@angular/core";
+import { Injectable, EventEmitter } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
@@ -6,15 +6,17 @@ import { map } from "rxjs/operators";
 @Injectable({
   providedIn: 'root'
 })
+
 export class NewsapiService {
+  countryChange: EventEmitter<any> = new EventEmitter();
   readonly apiUrl: string = "http://newsapi.org/v2/";
-  readonly apiKey: string = "&apiKey=8f1e637c0b0f47688a6850ff17433a78"
+  readonly apiKey: string = "&apiKey=7f3816ce018f4311b7b570904538eaf4"
   public apiSource: string = "top-headlines"
-  public apiCountry: string = "br"
+  public apiCountry: string = "BR"
   public apiCategory: string = "general"
   public apiCategories: string[] = ["entertainment", "sports", "business", "health", "science", "technology"];
   public apiCountries: string[] = ['ae', 'ar', 'at', 'au', 'be', 'bg', 'br', 'ca', 'ch', 'cn', 'co', 'cu', 'cz', 'de', 'eg', 'fr', 'gb', 'gr', 'hk', 'hu', 'id', 'ie', 'il', 'in', 'it', 'jp', 'kr', 'lt', 'lv', 'ma', 'mx', 'my', 'ng', 'nl', 'no', 'nz', 'ph', 'pl', 'pt', 'ro', 'rs', 'ru', 'sa', 'se', 'sg', 'si', 'sk', 'th', 'tr', 'tw', 'ua', 'us', 've', 'za']
-
+  
   constructor(private http: HttpClient) { }
 
   public getNews(country: string, category: string): Observable<any> {
@@ -24,7 +26,8 @@ export class NewsapiService {
   }
 
   public changeCountry(country: string) {
-    console.log('Aqui que a merda do pais tinha que trocar para "' + country + '"!');
+    this.countryChange.emit(country)
+    this.apiCountry = country
   }
 
 }

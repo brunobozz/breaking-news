65d241cf3c55866fa2c28f83f674f7c81846b0c6
brunobozz <brunobozz@gmail.com>
2020-04-27 05:59:18 -0300

    infos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,10 +5,10 @@
 export const environment = {
   production: false,
 
-  GCS_API_KEY: 'AIzaSyCCd4Fd_bk7Jtasj__wcFBpLiBv1DFtG14',
+  GCS_API_KEY: 'YOUR Google Custom Search API KEY',
   GCS_API_URL: 'https://www.googleapis.com/customsearch/v1',
-  GCS_ID: '015802847019070480802:oeqyhjdsfkt',
+  GCS_ID: 'YOUR Google Custom Search ID',
 
   NEWS_API_URL: "http://newsapi.org/v2/",
-  NEWS_API_KEY: "7f3816ce018f4311b7b570904538eaf4"
+  NEWS_API_KEY: "YOUR NEWS KEY"
 };

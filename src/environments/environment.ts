07ce67fdd1c61f443c0cb45459a0f87a0ec3f41b
bruno.bozzetti <bruno.bozzetti@dbserver.com.br>
2021-01-28 15:43:17 -0300

    comit teste

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,5 +10,5 @@ export const environment = {
   GCS_ID: 'YOUR Google Custom Search ID',
 
   NEWS_API_URL: "http://newsapi.org/v2/",
-  NEWS_API_KEY: "https://github.com/brunobozz/breaking-news.git"
+  NEWS_API_KEY: "7f3816ce018f4311b7b570904538eaf4"
 };

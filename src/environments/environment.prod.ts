e6d8d3c9eb755aa9d3cb2cad457e0795d1d79388
bruno.bozzetti <bruno.bozzetti@dbserver.com.br>
2021-01-28 15:43:46 -0300

    comit teste

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+
+  GCS_API_KEY: 'YOUR Google Custom Search API KEY',
+  GCS_API_URL: 'https://www.googleapis.com/customsearch/v1',
+  GCS_ID: 'YOUR Google Custom Search ID',
+
+  NEWS_API_URL: "http://newsapi.org/v2/",
+  NEWS_API_KEY: "7f3816ce018f4311b7b570904538eaf4"
 };

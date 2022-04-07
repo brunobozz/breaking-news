a6f1d3736a0fafc5b7f3f0bc3c691c47c17c286a
bruno.bozzetti <bruno.bozzetti@dbserver.com.br>
2022-04-07 14:40:57 -0300

    env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  GCS_API_KEY: "YOUR Google Custom Search API KEY",
+  GCS_API_KEY: "@GCS_API_KEY",
   GCS_API_URL: "https://www.googleapis.com/customsearch/v1",
-  GCS_ID: "YOUR Google Custom Search ID",
+  GCS_ID: "@GCS_ID",
 
   NEWS_API_URL: "http://newsapi.org/v2/",
-  NEWS_API_KEY: "YOUR NEWS KEY",
+  NEWS_API_KEY: "@NEWS_API_KEY",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,5 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
-
   GCS_API_KEY: 'AIzaSyCCd4Fd_bk7Jtasj__wcFBpLiBv1DFtG14',
   GCS_API_URL: 'https://www.googleapis.com/customsearch/v1',
   GCS_ID: '015802847019070480802:oeqyhjdsfkt',
